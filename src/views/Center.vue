91c696952c63f9b44bdaa518fe7ea39e14c17263
IDLbb <494435175@qq.com>
2019-02-22 11:46:38 +0800

    第一次提交修改

--- src/views/Center.vue
+++ src/views/Center.vue
@@ -1,5 +1,14 @@
 <template>
     <div>
-        个人中心
+        <h1>我是个人中心页</h1>
+        <MzFooter></MzFooter>
     </div>
 </template>
+<script>
+import MzFooter from '../components/MzFooter/index.vue';
+export default {
+    components: {
+        MzFooter
+    }
+}
+</script>
--- src/views/Cinema.vue
+++ src/views/Cinema.vue
@@ -1,5 +1,14 @@
 <template>
     <div>
-        影院
+        <h1>我是影院页</h1>
+        <MzFooter></MzFooter>
     </div>
 </template>
+<script>
+import MzFooter from '../components/MzFooter/index.vue';
+export default {
+    components: {
+        MzFooter
+    }
+}
+</script>
